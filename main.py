0ffc5d728988ec011eec0479aa22b3bc322d2771
kartikayakulshrestha <kartikaya.kul0908@gmail.com>
2023-08-13 19:44:19 +0530

    resizing the dataset according to 100x100with rgb

--- main.py
+++ main.py
@@ -7,5 +7,22 @@ from keras.models import Sequential
 import numpy as nump
 
 #Going to do binary image classification with cnn with tensorflow 
-print(tf.__version__,nump.__version__)
 
+
+#importing images here
+L_train= nump.loadtxt("Image Classification CNN Keras Dataset/input.csv" , delimiter=",")
+M_train= nump.loadtxt("Image Classification CNN Keras Dataset/labels.csv" , delimiter=",")
+
+L_test=nump.loadtxt("Image Classification CNN Keras Dataset/input_test.csv" , delimiter=",")
+M_test=nump.loadtxt("Image Classification CNN Keras Dataset/labels_test.csv" , delimiter=",")
+
+#resizing them in
+L_train=nump.reshape(len(L_train),100,100,3)
+M_train=nump.reshape(len(M_train),1)
+
+L_test=nump.reshape(len(L_test),100,100,3)
+M_test=nump.reshape(len(M_test),1)
+
+
+
+print(L_train.shape)
