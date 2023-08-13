6119f230b9d251a4b5b43ecf73aedcb4ee4c60ff
kartikayakulshrestha <kartikaya.kul0908@gmail.com>
2023-08-13 19:02:07 +0530

    installed all essentials:tensor,numpy,keras

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-import tensorflow as tk
-print("hello")
-print(tk.__version__)
+import tensorflow as tf
+#usually tensorflow.keras.models hota hai. but we are writing so 
+#because we have the keras file directly installed
+from keras.layers import Conv2D,MaxPool2D,Flatten,Dense
+from keras.models import Sequential
+
+import numpy as nump
+
+#Going to do binary image classification with cnn with tensorflow 
+print(tf.__version__,nump.__version__)
+
